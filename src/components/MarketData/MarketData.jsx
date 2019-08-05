43182ee2fc719cc74dde24563b07ea7239faf06d
Nune <nunedarb@gmail.com>
2019-08-05 22:11:44 +0400

    Adding symbols after currency code

--- src/components/MarketData/MarketData.jsx
+++ src/components/MarketData/MarketData.jsx
@@ -13,16 +13,50 @@ export default class MarketData extends Component {
       // debugger;
       return (
         <div className="marketData">
-          <ul>
+          <div className="base__wrapper">
+            <h1 className="base">
+              Base <span className="base__value">USD</span>
+            </h1>
+          </div>
+          <ul className="data__header">
+            <li className="fx fx-center-v data__headerItem">
+              <span className="data__headerCol data__headerCol1">
+                Currency Name
+              </span>
+              <span className="data__headerCol data__headerCol2">Code</span>
+              <span className="data__headerCol data__headerCol3">Price</span>
+              <span className="data__headerCol data__headerCol4">Volume</span>
+            </li>
+          </ul>
+          <ul className="data__list">
             {marketData.map(item => (
-              <li key={`market_${item.fromCurrency}`}>
-                {`From Currency: ${item.fromCurrency}, Price: ${
-                  item.price
-                }, Volume: ${item.volume}, Sign: ${currencySymbol(
-                  item.fromCurrency
-                )} Currency Name: ${item.currencyName}`}
+              <li
+                key={`market_${item.fromCurrency}`}
+                className="fx fx-center-v data__listItem"
+              >
+                <span className="data__listCol data__listCol1">
+                  {item.currencyName}
+                </span>
+                <span className="data__listCol data__listCol2">
+                  {`${item.fromCurrency} (${currencySymbol(
+                    item.fromCurrency
+                  )})`}
+                </span>
+                <span className="data__listCol data__listCol3">
+                  {item.price}
+                </span>
+                <span className="data__listCol data__listCol4">
+                  {item.volume}
+                </span>
+                {/* {`${currencySymbol(item.fromCurrency)}`} */}
                 <strong
-                  className="addRemoveFavorite"
+                  className={
+                    localStorage
+                      .getItem("favorites")
+                      .includes(JSON.stringify(item))
+                      ? "addRemoveFavorite addRemoveFavorite--favorited"
+                      : "addRemoveFavorite"
+                  }
                   onClick={this.props.toggleFavorite.bind(this, item)}
                 >
                   {localStorage
@@ -44,8 +78,11 @@ export default class MarketData extends Component {
     const { marketData } = this.props;
     return (
       <>
-        <button onClick={this.props.sortData.bind(this, marketData)}>
-          Sort: {this.props.sorted ? "Ascending" : "Descending"}
+        <button
+          onClick={this.props.sortData.bind(this, marketData)}
+          className="sortBtn"
+        >
+          {this.props.sorted ? "Ascending" : "Descending"}
         </button>
         <div className="tabRow">{this.renderMarketData()}</div>
       </>
